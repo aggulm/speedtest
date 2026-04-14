1a35bcc78401188243a01d71c8aa87b949cb1bdf
aggulm <aggulmehtiyeva@gmail.com>
2026-04-14 17:11:33 +0400

    feat: add footer

---
+++ frontend/JS/faq.js
@@ -0,0 +1,12 @@
+let questionIcon = document.querySelector("#questionIcon");
+let faqSection = document.querySelector(".faq-content");
+function handleClick(e) {
+  faqSection.sty;
+}
+questionIcon.addEventListener("click", function (e) {
+  faqSection.classList.toggle("faq-section");
+});
+
+questionIcon.addEventListener("click", function (e) {
+  questionIcon.classList.toggle("iconQuestion");
+});
