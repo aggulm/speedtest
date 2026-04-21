f4e688013582e06df1f6ac68dfe880ab5ce4a990
ayselmmmdli2220-source <ayselmmmdli2220@gmail.com>
2026-04-21 15:31:38 +0400

    feat: add privacy tag popup

---
+++ frontend/JS/privacy.js
@@ -0,0 +1,6 @@
+let privacyContent=document.querySelector(".privacyP")
+let privacy=document.querySelector(".privacy")
+
+privacy.addEventListener("click", function(e){
+    privacyContent.classList.toggle("privacyP")
+})
